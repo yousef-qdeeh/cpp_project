316a2f1f55f915a515887352bb1fcab14c8e514e
yousef-qdeeh <qdeehps@gmail.com>
2026-08-27 14:44:18 +0300

    Fix main.cpp

--- cpp_project_for_vscode/src/main.cpp
+++ cpp_project_for_vscode/src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cstdlib>
 #include "lib/store.h"
 using namespace store;
 using namespace std;
 int main(){
     mainActivityApp();
-    srand(unsigned(time(NULL));
+    srand(unsigned(time(NULL)));
 }
